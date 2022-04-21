d1dd930471a6c9e6e050191fc088256eb76d63e9
jelena.petkovic <jelena.petkovic@marbleit.rs>
2022-04-21 17:24:33 +0200

    Add unit tests for app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,20 +1,6 @@
 import { render } from '@testing-library/react';
 import App from './App';
-import Header from './common/header';
 
-test('renders app elements', () => {
+test('renders app', () => {
   render(<App />);
-  render(<Header />);
-});
-
-// it("renders App header", () => {
-//   const wrapper = shallow(<App />);
-//   const header = <Header/>;
-//   expect(wrapper.contains(header)).toEqual(true);
-// });
-
-test('renders board', () => {
-  //render(<Board />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
 });
---
+++ src/board/index.test.tsx
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from '../store';
+import Board from './';
+import { parseBoard } from './utils';
+
+test('renders board', () => {
+  render(
+    <Provider store={store}>
+        <Board/>
+    </Provider>
+  );
+});
+
+test('test parseBoard function', () => {
+
+    var boardString = "map:\n123\n456\n789"; 
+    const boardArray = [["1","2","3"],["4","5","6"],["7","8","9"]]; 
+
+    expect(parseBoard(boardString)).toStrictEqual(boardArray);
+})
